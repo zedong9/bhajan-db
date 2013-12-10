a230566ad99c8d03068347f9b30219e21b7cc488
Saish Gunturi <sai.gunturi@gmail.com>
2013-12-10 01:15:20 -0600

    Configure tasks to deploy to production.
    - Add grunt-git dependency.
    - Add checkout sub-tasks.
    - Configure and merge release tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,30 @@ module.exports = function (grunt) {
                     repo: 'sgunturi/bhajan-db'
                 }
             }
+        },
+        gitcheckout: {
+            production: {
+                options: {branch: 'production', create: false}
+            },
+            master: {
+                options: {branch: 'master', create: false}
+            }
+        },
+        gitpush: {
+            options: {
+                tags: true
+            }
         }
     });
 
     // Load our tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-release');
+    grunt.loadNpmTasks('grunt-git');
 
     // Register our tasks.
     grunt.registerTask('lint', 'jshint:default');
+    grunt.registerTask('lint-master', ['gitcheckout:master', 'lint']);
+    grunt.registerTask('production', ['lint-master', 'release', 'gitpush']);
+    grunt.registerTask('production-minor', ['lint-master', 'release:minor', 'gitpush']);
 };
