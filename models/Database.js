7fab134e5f262ab3faf301b7ee0dd71caae4fb13
Saish Gunturi <sai.gunturi@gmail.com>
2013-12-28 23:13:48 -0600

    Miscellaneous updates.
    - Re-enable route logging and simplify error handling.
    - Simplify bhajan method callbacks.
    - Don't restrict bhajan methods to approved only.
    - Search only approved bhajans by default but allow unapproved override.
    - Add tests for error handlers and unapproved search.
    - Add detail to script start output.
    - Throw error if database credentials are not provided in production
      mode.
    - Formatting.

--- models/Database.js
+++ models/Database.js
@@ -6,6 +6,7 @@ var mongoURI;
 if (process.env.TRAVIS_SECURE_ENV_VARS) {
     mongoURI = 'mongodb://127.0.0.1:27017/test';
 } else if (process.env.NODE_ENV === 'production') {
+    if (!process.env.DB_USER || !process.env.DB_SECRET) throw new Error('Database credentials not provided.');
     mongoURI = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_SECRET + '@paulo.mongohq.com:10080/bhajan-db';
 } else {
     var credentials = require('./db-credentials.json');
@@ -17,20 +18,13 @@ module.exports = {
     * Connect: Accepts collection name, callback. Returns
     * server connection and requested MongoDB Collection.
     */
-    connect: function (collection_name, callback) {
-        if (!_.isFunction(callback)) {
-            callback(new Error('Callback is not a function.'));
-            return;
-        } else {
-            mongodb.MongoClient.connect(mongoURI, function (error, database) {
-                if (error) {
-                    callback(error);
-                    return;
-                } else {
-                    var collection = new mongodb.Collection(database, collection_name);
-                    callback(null, database, collection);
-                }
-            });
-        }
+    connect: function (collection_name, next) {
+        if (!_.isFunction(next)) return new Error('Callback is not a function.');
+        mongodb.MongoClient.connect(mongoURI, function (error, database) {
+            if (error) return next(error);
+
+            var collection = new mongodb.Collection(database, collection_name);
+            next(null, database, collection);
+        });
     }
 };
--- test/App.js
+++ test/App.js
@@ -1,7 +1,11 @@
 var expect = require('chai').expect;
-var request = require('superagent');
 var http = require('http');
+var uuid = require('node-uuid');
+var request = require('superagent');
+
 var app = require('../app');
+
+var address = 'localhost:' + app.get('port');
 
 // Allow jshint to pass.
 /*jshint expr: true*/
@@ -15,10 +19,24 @@ describe('app.js', function () {
         });
     });
 
-    it('responds with 200 code.', function () {
-        request.get('localhost:3000').end(function (res) {
+    it('responds with 200 code on the homepage.', function () {
+        request.get(address).end(function (res) {
             expect(res).to.exist;
             expect(res.status).to.equal(200);
+        });
+    });
+
+    it('throws a 404 error if page is not found.', function () {
+        request.get(address + '/' + uuid.v4()).end(function (res) {
+            expect(res).to.exist;
+            expect(res.status).to.equal(404);
+        });
+    });
+
+    it('throws a 500 error for all other errors.', function () {
+        request.get(address + '/error').end(function (res) {
+            expect(res).to.exist;
+            expect(res.status).to.equal(500);
         });
     });
 });
