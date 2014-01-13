091f5f408378db83bd3d5cfa9364ca1d19fa9f41
Saish Gunturi <sai.gunturi@gmail.com>
2014-01-12 23:48:24 -0600

    Add functionality to edit bhajans as admin.
    - Add edit view and add edit link to bhajan view if logged in.
    - Add routes for editing bhajans.
    - Backup database.
    - Misc: Exclude test values from API results; allow test suite to run
      while server is running locally.

--- test/App.js
+++ test/App.js
@@ -5,7 +5,8 @@ var request = require('superagent');
 
 var app = require('../app');
 
-var address = 'localhost:' + app.get('port');
+var port = 3030;
+var address;
 
 // Allow jshint to pass.
 /*jshint expr: true*/
@@ -13,30 +14,35 @@ var address = 'localhost:' + app.get('port');
 describe('app.js', function () {
 
     before(function (done) {
-        http.createServer(app).listen(3000, function (error, server) {
-            if (error) console.log(error);
+        app.set('port', port);
+        address = 'localhost:' + app.get('port');
+        http.createServer(app).listen(app.get('port'), function (error, server) {
+            if (error) return done(error);
             done();
         });
     });
 
-    it('responds with 200 code on the homepage.', function () {
+    it('responds with 200 code on the homepage.', function (done) {
         request.get(address).end(function (res) {
             expect(res).to.exist;
             expect(res.status).to.equal(200);
+            done();
         });
     });
 
-    it('throws a 404 error if page is not found.', function () {
+    it('throws a 404 error if page is not found.', function (done) {
         request.get(address + '/' + uuid.v4()).end(function (res) {
             expect(res).to.exist;
             expect(res.status).to.equal(404);
+            done();
         });
     });
 
-    it('throws a 500 error for all other errors.', function () {
+    it('throws a 500 error for all other errors.', function (done) {
         request.get(address + '/error').end(function (res) {
             expect(res).to.exist;
             expect(res.status).to.equal(500);
+            done();
         });
     });
 });
