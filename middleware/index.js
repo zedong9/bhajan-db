e95d04e809aa7d906fe8904b2a98d9f69bb17ff3
Saish Gunturi <sai.gunturi@gmail.com>
2014-01-15 03:05:46 -0600

    Reconfigure middleware to catch errors correctly.

--- middleware/index.js
+++ middleware/index.js
@@ -7,9 +7,11 @@ module.exports = {
         if (req.isAuthenticated()) res.locals.username = req.user.username;
         next();
     },
-    errorHandler: function (err, req, res, next) {
-        if (err.status === 404) res.status(404).render('404');
-        else res.status(500).render('500');
-        console.log(err);
+    errorHandler: function () {
+        return function (err, req, res, next) {
+            if (err.status === 404) res.status(404).render('404');
+            else res.status(500).render('500');
+            console.log(err);
+        };
     }
 };
